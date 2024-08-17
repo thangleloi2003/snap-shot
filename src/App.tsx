498724162722bc569c29b12a147c6133cc403062
Thang Nguyen <thangleloi2003@gmail.com>
2024-08-17 18:12:51 +0700

    add func filter and fix error img

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
 import CategoryButtons from './components/CategoryButtons';
 
 export default function App() {
   return (
     <div>
       <Header />
-      <SearchBar />
       <CategoryButtons />
     </div>
   );
--- src/components/CategoryButtons.tsx
+++ src/components/CategoryButtons.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from '@nextui-org/react';
+import { Button, Input, Spinner } from '@nextui-org/react';
 import axios from 'axios';
-
 
 const categories = ['Ethereum', 'Arbitrum', 'Optimism', 'BSC'];
 
@@ -15,79 +14,105 @@ const tokenLists: { [key: string]: string } = {
 export type GetTokensResponseData = Token[]
 
 export interface Token {
-  symbol: string
-  name: string
-  address: string
-  decimals: number
-  chainId: number
-  logoURI: string
-  coingeckoId?: string
-  listedIn: string[]
+  symbol: string;
+  name: string;
+  address: string;
+  decimals: number;
+  chainId: number;
+  logoURI: string;
+  coingeckoId?: string;
+  listedIn: string[];
 }
 
+const defaultToken = 'https://e7.pngegg.com/pngimages/710/778/png-clipart-question-mark-question-mark.png';
 
 const CategoryButtons: React.FC = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>('Ethereum');
-  const [images, setImages] = useState<string[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
+  const [tokens, setTokens] = useState<Token[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
   useEffect(() => {
-    const fetchImages = async () => {
+    const fetchTokens = async () => {
       try {
         setLoading(true);
-        const response = await axios.get<GetTokensResponseData>(tokenLists[selectedCategory]);
-        console.log('response.data', response.data);
-        setImages(response.data.map(obj => obj.logoURI))
+        const timeoutId = setTimeout(() => {
+          setLoading(false);
+        }, 5000);
+        const response = await axios.get<Token[]>(tokenLists[selectedCategory]);
+        clearTimeout(timeoutId); // Xóa timeout khi có dữ liệu
+        const checkedTokens = response.data.map((token, index) => {
+          if (selectedCategory === 'Arbitrum' && [112, 115, 137].includes(index + 1)) {
+            return {
+              ...token, logoURI: defaultToken,
+            };
+          }
+          return token;
+        });
+        setTokens(checkedTokens);
+
         setLoading(false);
       } catch (err) {
-        setError((err as Error).message);
         setLoading(false);
       }
     };
-    fetchImages();
+    fetchTokens();
   }, [selectedCategory]);
 
   const handleCategoryChange = (category: string) => {
     setSelectedCategory(category);
   };
 
+  const filteredTokens = tokens.filter((token) =>
+    token.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    token.symbol.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleTokenError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    event.currentTarget.src = defaultToken;
+  };
+
   return (
     <div>
+      <div className="search-form flex justify-center">
+        <Input type="text" placeholder="Search..." className="rounded-none p-0 auto w-[369px]" 
+        value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Button className="min-w-fit w-16 h-10 rounded-md cursor-pointer bg-[#051c33]" disabled>
+          <svg height="32" width="32"><path d="M19.427 21.427a8.5 8.5 0 1 1 2-2l5.585 5.585c.55.55.546 1.43 0 1.976l-.024.024a1.399 1.399 0 0 1-1.976 0l-5.585-5.585zM14.5 21a6.5 6.5 0 1 0 0-13 6.5 6.5 0 0 0 0 13z" fill="#ffffff" fill-rule="evenodd"></path>
+          </svg>
+        </Button>
+      </div>
+
       <div className="flex justify-center m-[40px] space-x-4 sm:space-x-2">
         {categories.map((category) => (
-          <Button
-            key={category}
-            className="w-[96px] h-[28px] px-4 bg-[#051C33] text-white rounded text-[16px]"
-            onClick={() => handleCategoryChange(category)}
-          >
+          <Button key={category} className="w-[96px] h-[28px] px-4 bg-[#051C33] text-white rounded text-[16px]"onClick={() => handleCategoryChange(category)}>
             {category}
           </Button>
         ))}
       </div>
+
       <div className="w-[1200px] mx-auto px-5 pb-8 token-gallery">
         <h2 className="text-center text-[32px] text-[#051c33] font-semibold mt-[52px] mx-0 mb-8">
           {selectedCategory} Token Library
         </h2>
 
         {loading ? (
-          <p>Loading...</p>
+          <div className="flex justify-center items-center h-80">
+          <Spinner size="lg" color="primary" />
+        </div>
         ) : (
-          <div className="flex flex-wrap justify-center gap-5">
-            {images && images.length > 0 ? (
-              images.map((url, index) => (
-                <div key={index} className="w-[220px] h-[220px] relative overflow-hidden cursor-pointer transition-transform duration-700 ease-in-out hover:scale-110 hover:shadow-lg token-main">
-                  <img src={url} alt={`Token ${index + 1}`} className="w-full h-full object-cover" />
+          <div className="flex flex-wrap justify-center gap-5 photo">
+            {filteredTokens && filteredTokens.length > 0 ? (filteredTokens.map((url, index) => (
+                <div key={index} className="w-28 h-28 relative photo-main overflow-hidden cursor-pointer transition-transform duration-700 ease-in-out hover:scale-110 hover:shadow-lg border-none photo-main">
+                  <img src={url.logoURI} alt={`Token ${index + 1}`} className="w-full h-full object-cover" onError={handleTokenError}
+                  />
                 </div>
               ))
             ) : (
               <p>No tokens available for this category.</p>
             )}
           </div>
-        )}
-
-        {error && (
-          <p style={{ color: 'red' }}>{error}</p>
         )}
       </div>
     </div>
